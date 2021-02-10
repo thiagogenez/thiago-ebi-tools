eaef2cbc88dbec742586930c2b340c61a43d4e01
Thiago Genez <thiagogenez@ebi.ac.uk>
2021-02-10 13:58:43 +0000

    update log file extractor

--- bin/log-extracter.py
+++ bin/log-extracter.py
@@ -5,38 +5,49 @@ import argparse
 import os.path
 import sys
 
+import datetime
+
 from collections import OrderedDict
 from statistics import mean, pstdev
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input", help = "log file")
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("input", help = "log file")
+  args = parser.parse_args()
 
-    if not os.path.exists(args.input):
-        print('File {} does not exist!'.format(args.input))
-        sys.exit(1)
+  if not os.path.exists(args.input):
+    print('File {} does not exist!'.format(args.input))
+    sys.exit(1)
 
-    table = OrderedDict()
+  table = OrderedDict()
 
-    for line in fileinput.input(args.input):
-        if 'Successfully ran:' in line:
-            line = (line.strip())
+  for line in fileinput.input(args.input):
+    if 'Successfully ran:' in line:
+      line = (line.strip())
 
-            line = line.split('ran:')[1].split()
-            job = line[0]
+      line = line.split('ran:')[1].split()
+      job = line[0].replace('"','')
 
-            assert line[-1] == 'seconds'
-            time = float(line[-2])
+      assert line[-1] == 'seconds'
+      time = float(line[-2])
 
-            if job in table:
-                table[job].append(time)
-            else:
-                table[job] = [time]
+      if job in table:
+        table[job].append(time)
+      else:
+        table[job] = [time]
 
 
+  total = 0.0
+  for item in table.keys():
+    print('{};{};{:.2f};{:.2f}'.format(item,
+      len(table[item]),
+      mean(table[item]),
+      pstdev(table[item])
+      ))
+    
+    total = total + sum(table[item])
+    
+  print('Runtime = {} seconds or {}'.format(total, str(datetime.timedelta(seconds=total))))
 
-    for item in table.keys():
-        print('Job {} executed {} times with a duration of {:.2f} seconds [+- {:.2f} seconds]'.format(item,len(table[item]),mean(table[item]), pstdev(table[item])))
