b24ee7ad706de7582d70e5bb88553aad579b69a3
Thiago Genez <thiagogenez@ebi.ac.uk>
2021-04-10 00:25:14 +0100

    Update cpu-and-gpu-utilisation.sh

--- bin/cpu-and-gpu-utilisation.sh
+++ bin/cpu-and-gpu-utilisation.sh
@@ -7,17 +7,21 @@ fi
 
 CSV_FILE=$1
 
-start_time="$(date +%s.%N)"
-echo "TIME,CPU_USAGE,GPU_USAGE" >> $CSV_FILE
+#start_time="$(date +%s.%N)"
+start_time=$SECONDS
+
+echo "TIME_SECONDS,TIME_FORMAT,CPU_USAGE,GPU_USAGE" >> $CSV_FILE
 
 while true
 do
 	CPU_USAGE=$(cat <(grep 'cpu ' /proc/stat) <(sleep 0.05 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
 	GPU_USAGE=$(nvidia-smi | grep "%" | awk '{print $13}' | cut -d'%' -f1  | awk '{ sum += $1 } END { print(sum / NR) }')
 
-	end_time="$(date +%s.%N)"
+	#end_time="$(date +%s.%N)"
+	end_time=$SECONDS
 	elapsed=$( echo "$end_time - $start_time" | bc -l )
-	echo "$elapsed,$CPU_USAGE,$GPU_USAGE" >> $CSV_FILE
+	format_time=$(TZ=UTC0 printf '%(%H:%M:%S)T\n' "$elapsed")
+	echo "$elapsed,$format_time,$CPU_USAGE,$GPU_USAGE" >> $CSV_FILE
 done
 
 
