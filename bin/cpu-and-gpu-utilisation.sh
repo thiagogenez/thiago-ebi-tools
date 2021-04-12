b1ab810429c67d1f52de197e5e4d35fa61264d6e
Thiago Genez <thiagogenez@ebi.ac.uk>
2021-04-12 14:08:04 +0100

    Update cpu-and-gpu-utilisation.sh

--- bin/cpu-and-gpu-utilisation.sh
+++ bin/cpu-and-gpu-utilisation.sh
@@ -14,8 +14,9 @@ echo "TIME_SECONDS,TIME_FORMAT,CPU_USAGE,GPU_USAGE" >> $CSV_FILE
 
 while true
 do
-	CPU_USAGE=$(cat <(grep 'cpu ' /proc/stat) <(sleep 0.05 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
+	# GPU parser is slower, so get it first
 	GPU_USAGE=$(nvidia-smi | grep "%" | awk '{print $13}' | cut -d'%' -f1  | awk '{ sum += $1 } END { print(sum / NR) }')
+	CPU_USAGE=$(cat <(grep 'cpu ' /proc/stat) <(sleep 0.05 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
 
 	#end_time="$(date +%s.%N)"
 	end_time=$SECONDS
