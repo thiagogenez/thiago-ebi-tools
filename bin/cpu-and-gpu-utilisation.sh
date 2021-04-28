e4984ca62212869d5cbd9c6bb80796aa91827de3
Thiago Genez <thiagogenez@ebi.ac.uk>
2021-04-28 14:01:25 +0100

    Update cpu-and-gpu-utilisation.sh

--- bin/cpu-and-gpu-utilisation.sh
+++ bin/cpu-and-gpu-utilisation.sh
@@ -13,17 +13,18 @@ get_elapsed_time(){
 
 start_time=$SECONDS
 while true; do
-    cactus_pid=$(ps axf | grep 'cactus-preprocess\|cactus-blast\|cactus-align\|run_segalign' | grep -v grep | awk '{print $1}')
-    if [[ "$cactus_pid" != "" ]]; then
-        break
-    fi
-    sleep 1
+   cactus_pid=$(ps axf | grep 'cactus-preprocess\|cactus-blast\|cactus-align\|run_segalign' | grep -v grep | awk '{print $1}')
+   if [[ "$cactus_pid" != "" ]]; then
+      echo "found PID=$cactus_pid"
+      break
+   fi
+   sleep 1
 
-    elapsed=$(get_elapsed_time $start_time)
-    if [[ "$elapsed" -gt "30" ]]; then
-        echo "Cactus PID not found, exiting with code 1"
-        exit 1
-    fi
+   elapsed=$(get_elapsed_time $start_time)
+   if [[ "$elapsed" -gt "30" ]]; then
+      echo "Cactus PID not found, exiting with code 1"
+      exit 1
+   fi
 
 done
 
@@ -32,29 +33,32 @@ start_time=$SECONDS
 
 echo "TIME_SECONDS,TIME_FORMAT,CPU_USAGE,CPU_USAGE2,GPU_USAGE,MEM_USAGE,GPU_MEM_USAGE" >> $CSV_FILE
 
-nvidia_file="nvidia-$(hostname).log"
+#nvidia filename
+NVIDIA_LOG="nvidia-$(hostname).log"
+[ -f $NVIDIA_LOG ] && rm $NVIDIA_LOG
+
 while true; do
-        # GPU parser is slower, so get it first
-        nvidia-smi > $nvidia_file
+   # GPU parser is slower, so get it first
+   nvidia-smi > $NVIDIA_LOG
 
-        GPU_USAGE=$(grep "%" nvidia.log | awk '{print $13}' | cut -d'%' -f1  | awk '{ sum += $1 } END {  if (NR > 0) print(sum / NR) }')
-        GPU_MEM_USAGE=$(grep "MiB" $nvidia_file | head -4 | sed 's/MiB//g' | awk '{printf "%f\n", ($9/$11)*100 }' | awk '{ total += $1 } END {  if (NR > 0) print total/NR }')
-        MEM_USAGE=$(free -t | awk 'FNR == 2 {print ($3/$2)*100}')
-        CPU_USAGE=$(top -b -n2 -d 0.01 | fgrep "Cpu(s)" | tail -1 | gawk '{print $2+$4+$6}')
-        CPU_USAGE_2=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
+   GPU_USAGE=$(grep "%" $NVIDIA_LOG | awk '{print $13}' | cut -d'%' -f1  | awk '{ sum += $1 } END {  if (NR > 0) print(sum / NR) }')
+   GPU_MEM_USAGE=$(grep "MiB" $NVIDIA_LOG | head -4 | sed 's/MiB//g' | awk '{printf "%f\n", ($9/$11)*100 }' | awk '{ total += $1 } END {  if (NR > 0) print total/NR }')
+   MEM_USAGE=$(free -t | awk 'FNR == 2 {print ($3/$2)*100}')
+   CPU_USAGE=$(top -b -n2 -d 0.01 | fgrep "Cpu(s)" | tail -1 | gawk '{print $2+$4+$6}')
+   CPU_USAGE_2=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
 
 
-        elapsed=$(get_elapsed_time $start_time)
-        format_time=$(TZ=UTC0 printf '%(%H:%M:%S)T\n' "$elapsed")
+   elapsed=$(get_elapsed_time $start_time)
+   format_time=$(TZ=UTC0 printf '%(%H:%M:%S)T\n' "$elapsed")
 
-        echo "$elapsed,$format_time,$CPU_USAGE,$CPU_USAGE_2,$GPU_USAGE,$MEM_USAGE,$GPU_MEM_USAGE" >> $CSV_FILE
+   echo "$elapsed,$format_time,$CPU_USAGE,$CPU_USAGE_2,$GPU_USAGE,$MEM_USAGE,$GPU_MEM_USAGE" >> $CSV_FILE
 
-        rm $nvidia_file
+   rm $NVIDIA_LOG
 
-        # if cactus process is dead, exit the script
-        ps -p $cactus_pid > /dev/null || break
+   # if cactus process is dead, exit the script
+   ps -p $cactus_pid > /dev/null || break
 
-        sleep 0.5
+   sleep 0.5
 done
 
 echo "$(basename $0) finalised"
