eb568ca8921ab195d816280ff061b469f4765ed2
Thiago Genez <thiagogenez@ebi.ac.uk>
2021-07-05 18:05:30 +0100

    Improvements for cactus_consolidated top command

--- bin/usage.sh
+++ bin/usage.sh
@@ -56,7 +56,7 @@ if [[ "$CSV_FILE" == "" ]]; then
 fi
 
 
-CVS_FILE_2="$(echo ${CSV_FILE})-top.txt"
+CSV_FILE_2="$(echo ${CSV_FILE})-top.txt"
 echo "Given arguments:"
 echo "  FILE CSV_FILE   = ${CSV_FILE}"
 echo "  FILE CSV_FILE 2 = ${CSV_FILE_2}"
@@ -107,7 +107,6 @@ else
 fi
 
 
-
 while true; do
    
   if [[ "$GPU" == "YES" ]]; then
@@ -121,24 +120,39 @@ while true; do
     sleep 5
   fi
 
+  #SYSTEM-WIDE
   MEM_USAGE=$(free -t | awk 'FNR == 2 {print ($3/$2)*100}')
-  CPU_USAGE=$(top -b -n2 -d 0.01 | fgrep "Cpu(s)" | tail -1 | gawk '{print $2+$4+$6}')
-  CPU_USAGE_2=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
+  CPU_USAGE=$(top -b -n 2 -d 0.5 | fgrep "Cpu(s)" | tail -1 | gawk '{print $2+$4+$6}')
+  CPU_USAGE_2=$(cat <(grep 'cpu ' /proc/stat) <(sleep 0.5 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
 
 
-  ps -o user,pid,ppid,ni,rss,sz,vsz,%cpu,%mem,state,pagein,etime,cmd --width=2048 >> ${CSV_FILE_2}
-  CPU_CONSOLIDATED=$(cat ${CSV_FILE_2} | grep cactus_consolidated | awk 'END {print $8}')
-  MEMORY_CONSOLIDATED=$(cat ${CSV_FILE_2} | grep cactus_consolidated | awk 'END {print $9}')
+  # cactus-consolidated
+  if [[ -z $consolidated_pid ]]; then
+    consolidated_pid=$(ps axf | grep 'cactus_consolidated' | grep -v grep | awk '{print $1}') 
+  fi
+
+  #ps -o user,pid,ppid,ni,rss,sz,vsz,%cpu,%mem,state,pagein,etime,cmd --width=2048 >> ${CSV_FILE_2}
+  #CPU_CONSOLIDATED=$(cat ${CSV_FILE_2} | grep cactus_consolidated | awk 'END {print $8}')
+  #MEMORY_CONSOLIDATED=$(cat ${CSV_FILE_2} | grep cactus_consolidated | awk 'END {print $9}')
 
   elapsed=$(get_elapsed_time $start_time)
   format_time=$(TZ=UTC0 printf '%(%H:%M:%S)T\n' "$elapsed")
 
   if [[ "$GPU" == "YES" ]]; then
 
-    echo "$elapsed,$format_time,$CPU_USAGE,$CPU_USAGE_2,$GPU_USAGE,$MEM_USAGE,$GPU_MEM_USAGE,$CPU_CONSOLIDATED,$MEMORY_CONSOLIDATED" >> $CSV_FILE
-
+    echo "$elapsed,$format_time,$CPU_USAGE,$CPU_USAGE_2,$GPU_USAGE,$MEM_USAGE,$GPU_MEM_USAGE" >> $CSV_FILE
     rm $NVIDIA_LOG
+
   else
+    if [[ ! -z $consolidated_pid ]]; then
+      CPU_CONSOLIDATED=0.0
+      MEMORY_CONSOLIDATED=0.0
+    else
+      top_consolidated=$(top -b -n 2 -d 0.2 -p $consolidated_pid | tail -1 )
+      CPU_CONSOLIDATED=$(echo $top_consolidated | awk '{print $9}' )
+      MEMORY_CONSOLIDATED=$(echo $top_consolidated | awk '{print $10}' )
+      ps -p $consolidated_pid > /dev/null || unset consolidated_pid
+    fi
     echo "$elapsed,$format_time,$CPU_USAGE,$CPU_USAGE_2,$MEM_USAGE,$CPU_CONSOLIDATED,$MEMORY_CONSOLIDATED" >> $CSV_FILE
   fi
 
