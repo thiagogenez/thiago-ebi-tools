0999963006acb58f23044da2bd0ba49763af31ef
Thiago Genez <thiagogenez@ebi.ac.uk>
2021-07-20 16:17:38 +0000

    commands parser done

--- bin/run-cactus.py
+++ bin/run-cactus.py
@@ -15,91 +15,92 @@ STRING_TABLE = {
     "hal2fasta": "hal2fasta",
     "merging": "## HAL merging",
     "alignment": "## Alignment",
+    "preprocessor": "## Preprocessor",
 }
 
 
-def prepare_round(steps_dir, jobstore_dir, round_path):
-    pathlib.Path(round_path).mkdir(parents=True, exist_ok=True)
+def create_symlinks(src_dirs, dest):
+    pathlib.Path(dest).mkdir(parents=True, exist_ok=True)
 
-    for i in [steps_dir, jobstore_dir]:
-        relativepath = os.path.relpath(i, round_path)
-        fromfolderWithFoldername = round_path + '/' + os.path.basename(i)
-        #print('round_path={}, i={}, os.path.basename(i)={}'.format(round_path, i, os.path.basename(i)))
+    for i in src_dirs:
+        relativepath = os.path.relpath(i, dest)
+        fromfolderWithFoldername = dest + '/' + os.path.basename(i)
+        #print('dest={}, i={}, os.path.basename(i)={}'.format(dest, i, os.path.basename(i)))
         #print('fromfolderWithFoldername: '+fromfolderWithFoldername) 
         #print('relativepath: '+ relativepath)
+        #exit(0)
         os.symlink(src=relativepath, dst=fromfolderWithFoldername)   
     
 
 def append(filename, line):
-    with open(filename, mode="a") as f:
-        f.write(line + '\n')
+    if line:
+        with open(filename, mode="a") as f:
+            if f.tell() > 0:
+                f.write('\n')
+            f.write(line.strip())
 
-def parse_preprocessor(lines, line_number, steps_dir, jobstore_dir, input_dir):
+def parse(read_func, symlink_dirs, task_dir, task_name, stop_condition):
 
-    while line_number < len(lines):
-        line = lines[line_number].strip()
-        
-        # empty line, next
-        if not line:
-            # go to the next line
-            line_number = line_number + 1
-            continue
-
-        # job done
-        if line.startswith(STRING_TABLE["alignment"]):
-            return line_number
-
-    raise Exception("ERROR: parsing Preprocessor step")
-
-
-def parse_alignment(lines, line_number, steps_dir, jobstore_dir, rounds_dir):
-
-    while line_number < len(lines):
-        line = lines[line_number].strip()
-        
-        # empty line, next
-        if not line:
-            # go to the next line
-            line_number = line_number + 1
-            continue
-
-        # job done
-        if line.startswith(STRING_TABLE["merging"]):
-            return line_number
-
-        # create a new round directory
-        if line.startswith(STRING_TABLE["round"]):
-            round_id = line.split()[-1]
-            round_path = "{}/rounds/{}".format(rounds_dir, round_id)
-            prepare_round(
-                steps_dir=steps_dir,
-                jobstore_dir=jobstore_dir,
-                round_path=round_path,
-            )
-
-            block_id = 0
-            
-            # go to the next line
-            line_number = line_number + 1
-            continue
-
-        # sanity check
-        assert "round_path" in locals()
-
-       	# get Anc_id from the current command-line
-        anc_id = re.findall('Anc[0-9]+', line)[0]
-        
-        # create block filename
-        block_filename = "{}/{}.txt".format(round_path, anc_id)
-
-        # write the current command-line in the file
-        append(filename=block_filename, line=line)
-
+    if 'alignment' not in task_name:
+        path = '{}/{}'.format(task_dir, task_name)
+        create_symlinks(src_dirs=symlink_dirs, dest=path)
+        commands_filename = '{}/commands.txt'.format(path)
+    
+    while True:
         # get the next line
-        line_number = line_number + 1
+        line = next(read_func, None)
+        
+        # job done: NONE
+        if not line:
+            break
 
-    raise Exception("ERROR: Parsing alignment step")
+        # strip the line
+        line = line.strip()
+        
+        # empty line, next
+        if not line:
+            continue       
 
+        # job done for task_name
+        if stop_condition and line.startswith(stop_condition):
+            break;
+
+       
+        if 'alignment' in task_name:
+            # create a new round directory
+            if line.startswith(STRING_TABLE["round"]):
+                round_id = line.split()[-1]
+                round_path = "{}/alignments/{}".format(task_dir, round_id)
+                create_symlinks(
+                    src_dirs=symlink_dirs,
+                    dest=round_path
+                )
+
+                # go to the next line
+                continue
+            
+            # sanity check
+            assert "round_path" in locals()
+
+       	    # get Anc_id from the current command-line
+            anc_id = re.findall('Anc[0-9]+', line)[0]
+        
+            # create block filename
+            commands_filename = "{}/{}.txt".format(round_path, anc_id)
+
+ 
+        # write the current command-line in the file
+        append(filename=commands_filename, line=line)
+
+
+
+def read_file(filename):
+    with open(filename, mode='r') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            yield line
 
 if __name__ == "__main__":
 
@@ -126,53 +127,56 @@ if __name__ == "__main__":
         help="Location of the jobstore directory",
     )
     parser.add_argument(
-        "--rounds_dir",
+        "--alignments_dir",
         metavar="PATH",
         type=str,
-        default=None,
+        default=os.getcwd(),
         required=False,
-        help="Location of the rounds directory",
+        help="Location of the aligment directory",
     )
     parser.add_argument(
-        "--preprocess-only",
-        metavar="True or False",
-        type=bool,
+        "--preprocessor_dir",
+        metavar="PATH",
+        type=str,
+        default=os.getcwd(),
         required=False,
-        default=False,
-        help="Prepare only cactus-preprocess command-line",
+        help="Location of the preprocessor directory",
     )
     parser.add_argument(
-        "--alignment-only",
-        metavar="True or False",
-        type=bool,
+        "--merging_dir",
+        metavar="PATH",
+        type=str,
+        default=os.getcwd(),
         required=False,
-        default=False,
-        help="Prepare only cactus-blast, cactus-align and command-lines",
+        help="Location of the merging directory",
     )
-
+    parser.add_argument(
+        "--input_dir",
+        metavar="PATH",
+        type=str,
+        required=True,
+        help="Location of the input directory",
+    )
     args = parser.parse_args()
 
-    with open(args.commands, mode="r") as f:
-        lines = f.readlines()
-
-        if args.rounds_dir is None:
-            args.rounds_dir = os.path.dirname(os.path.realpath(f.name))
-        else:
-            args.rounds_dir = os.path.abspath(args.rounds_dir)
-
+    
+    args.alignments_dir = os.path.abspath(args.alignments_dir)
     args.steps_dir = os.path.abspath(args.steps_dir)
     args.jobstore_dir = os.path.abspath(args.jobstore_dir)
+    args.input_dir = os.path.abspath(args.input_dir)
+    args.preprocessor_dir = os.path.abspath(args.preprocessor_dir)
+    args.merging_dir = os.path.abspath(args.merging_dir)
 
-    line_number = 0
-    while line_number < len(lines):
-        line = lines[line_number].strip()
-        if line.startswith(STRING_TABLE["round"]):
-            line_number = parse_alignment(
-                lines=lines,
-                line_number=line_number,
-                jobstore_dir=args.jobstore_dir,
-                steps_dir=args.steps_dir,
-                rounds_dir=args.rounds_dir,
-            )
-            
-        line_number = line_number + 1
+    read_func = read_file(args.commands)
+    while True:
+        line = next(read_func, '')
+        if not line:
+            break
+        if not line.startswith(STRING_TABLE['preprocessor']):
+            continue
+        
+        parse(read_func=read_func, symlink_dirs=[args.steps_dir, args.jobstore_dir, args.input_dir], task_name='preprocessor', task_dir=args.preprocessor_dir, stop_condition=STRING_TABLE['alignment'])   
+       
+        parse(read_func=read_func, symlink_dirs=[args.steps_dir, args.jobstore_dir], task_name='alignment', task_dir=args.alignments_dir, stop_condition=STRING_TABLE['merging'])
+        
+        parse(read_func=read_func, symlink_dirs=[args.steps_dir, args.jobstore_dir], task_name='merging', task_dir=args.merging_dir, stop_condition=None)   
